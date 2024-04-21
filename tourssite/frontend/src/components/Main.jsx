3ffab2417893c339c56d9dd762f9c78f7fd2b0fb
nereverine <lucas_stormborn@hotmail.com>
2024-04-21 15:49:15 +0100

    Main container 1

--- tourssite/frontend/src/components/Main.jsx
+++ tourssite/frontend/src/components/Main.jsx
@@ -1,7 +1,12 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
-import { Button } from "reactstrap";
+import Button from "@mui/material/Button";
 import {useNavigate, Link, Navigate} from "react-router-dom";
+import '../styles/main.css'
+import AddIcon from '@mui/icons-material/Add';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 document.title="Tours"
 
 let isLoggedIn = false
@@ -37,7 +42,25 @@ export default function Main(){
      <div>
     {/* If the user is logged in, render: */}
       {isLoggedIn ? (
-        <Button>Logged in</Button>
+
+    <div className="home-container">
+      <span className="home-text">My Tours</span>
+      <div className="home-container2 px-4">
+        <Button variant="contained" color="success" endIcon={<AddIcon />}>
+          Create Tour
+        </Button>
+      </div>
+      <div className="home-container3">
+        ola
+      </div>
+    </div>
+
+
+
+
+
+
+
       ) : (<></>)
       }
     </div>
